4ae12b954ef9af229720f9766eb2536b3f05b61b
hjs429 <hjs928@naver.com>
2020-11-04 17:15:29 +0900

    sytle: Add some annotations Additionally add_file function's initialdir style was changed

--- Image-Merge.py
+++ Image-Merge.py
@@ -9,13 +9,14 @@ from PIL import Image
 root = Tk()
 root.title("Image Merge")
 
+# 맨 윗줄에 프레임을 먼저 만들어줘야한다.
 #파일 프레임(파일 추가, 선택 삭제)
 file_frame = Frame(root)
 file_frame.pack(fill='x', padx = 5, pady = 5)
 
 #파일 추가 함수
 def add_file():
-    files = filedialog.askopenfilenames(title = '이미지 파일을 선택하세요',filetypes=(('PNG 파일','*.png'),('모든파일','*.*')), initialdir="C:/Users/Administrator/Desktop/IT 자료") #최초에 C:/ 경로를 보여준다.
+    files = filedialog.askopenfilenames(title = '이미지 파일을 선택하세요',filetypes=(('PNG 파일','*.png'),('모든파일','*.*')), initialdir=r"C:\Users\lemon\Desktop\Visual Studio\Phython\panggame\images") #최초에 C:/ 경로를 보여준다.
 
     # 사용자가 선택한 파일 목록
     for file in files:
@@ -26,7 +27,6 @@ def add_file():
 def del_file():
     for index in reversed(list_file.curselection()):
         list_file.delete(index)
-        g
 
 #저장 경로(폴더)
 def browse_save_path():
@@ -36,71 +36,30 @@ def browse_save_path():
     save_path.delete(0,END)
     save_path.insert(0, folder_selected)
 
-    #이미지 통합 함수
+    #이미지 통합
 def merge_image():
-
-    #가로 넓이
-    img_width = cmb_width.get()
-    if img_width == '원본유지':
-        img_width = -1
-    else:
-        img_width = int(img_width)
-
-    # 간격
-    img_space = cmb_space.get()
-    if img_space == '좁게':
-        img_space = 30
-    elif img_space == '보통':
-        img_space = 60
-    elif img_space == '넓게':
-        img_space = 90
-    else:# 없음
-        img_space = 0
-
-    #포맷
-    img_format = cmb_format.get().lower() # PNG, JPG, BMP 값을 받아와서 소문자로 변경
-
-
-
     #print(list_file.get(0,END)) # 모든 파일 목록을 가져오기
     images = [Image.open(x) for x in list_file.get(0, END)]
-    
-    # 이미지 사이즈 리스트에 넣어서 하나씩 처리
-    image_sizes = [] # (width1, height1),(width2, height2)
-    if img_width >-1:
-        # width 변경
-        image_sizes = [(int(img_width), int(img_width*x.size[1]/x.size[0])) for x in images]
-    else:
-        #원본 사이즈 사용
-        image_sizes = [(x.size[0], x.size[1]) for x in images]
-
-
-    widths, heights = zip(*(image_sizes))
+    widths = [x.size[0] for x in images]
+    heights = [x.size[1] for x in images]
 
     max_width, total_height = max(widths), sum(heights)
-
-    if img_space >0: #이미지 간격 옵션 적용
-        total_height += (img_space* (len(imgaes) -1))
 
     #스케치북 준비 
     result_img = Image.new('RGB', (max_width, total_height),(255,255,255))
     y_offset = 0 # y 위치정보
 
     for idx, img in enumerate(images):
-        #width가 원본이 아닐 때에는 이미지 크기를 조정해야 한다.
-        if img_width >-1:
-            img = img.resize(image_sizes[idx])
         result_img.paste(img,(0,y_offset))
-        y_offset += img.size[1] + img_space  #height 값 + 사용자가 지정한 간격
+        y_offset += img.size[1] #height만큼 더해주는 것이다.
 
         progress = (idx +1) / len(images) * 100 #실제 percent 정보를 계산
         p_var.set(progress)
         progress_bar.update()
     
-    # 포맷 옵션 처리
-    file_name = 'result_photo' + img_format
-    output_path = os.path.join(save_path.get(), file_name)
-    result_img.save(output_path)
+
+    dest_path = os.path.join(save_path.get(), 'nado_photo.jpg')
+    result_img.save(dest_path)
     msgbox.showinfo('알림','작업이 완료되었습니다.')
 
 
@@ -120,30 +79,36 @@ def start():
     merge_image()
 
 
+# 파일 프레임 안에 파일추가, 선택삭제 버튼을 넣어준다.
 btn_add_file = Button(file_frame, padx = 5, pady = 5, width = 12, text ='파일추가', command = add_file)
 btn_add_file.pack(side = 'left')
+
 btn_del_file = Button(file_frame, padx = 5, pady = 5, width = 12, text = '선택삭제', command = del_file)
 btn_del_file.pack(side = 'right')
 
-
+#파일 목록들이 들어가는 프레임을 만들어준다.
 #리스트 프레임
 list_frame = Frame(root)
 list_frame.pack(fill = 'both')
 
+#파일이 많을 경우 편리하게 보기 위해 스크롤바가 필요하다.
 scrollbar = Scrollbar(list_frame)
 scrollbar.pack(side = 'right', fill = 'y')
 
+#리스트 박스를 만들고 스크롤바 작동 명령어를 넣어준다.
 list_file = Listbox(list_frame, selectmode = 'extended', height = 15, yscrollcommand = scrollbar.set)
 list_file.pack(side = 'left', fill = 'both', expand = True)
 scrollbar.config(command = list_file.yview)
 
-#저장 경로 프레임
+#저장 경로를 만들기 위한 레이블 프레임을 만든다.
 path_frame = LabelFrame(root, text = '저장경로')
 path_frame.pack(fill='x', padx = 5, pady = 5)
 
+#저장 경로
 save_path = Entry(path_frame)
 save_path.pack(side = 'left', fill = 'x', expand = True, padx = 5, pady = 5)
 
+#찾아보기 버튼 추가
 btn_search_file = Button(path_frame, padx = 5, pady = 2, width = 6,text ='찾아보기', command = browse_save_path)
 btn_search_file.pack(side = 'right')
 
@@ -191,11 +156,11 @@ cmb_format.current(0)
 cmb_format.pack(side = 'left')
 
 #진행 상황 progressbar
-frame_progress = LabelFrame(root, text = '진행상황')
-frame_progress.pack(fill='x', padx = 5, pady = 5, ipady = 5)
+progress_frame = LabelFrame(root, text = '진행상황')
+progress_frame.pack(fill='x', padx = 5, pady = 5, ipady = 5)
 
 p_var = DoubleVar()
-progress_bar = ttk.Progressbar(frame_progress, maximum = 100, variable =p_var)
+progress_bar = ttk.Progressbar(progress_frame, maximum = 100, variable =p_var)
 progress_bar.pack(fill='x', padx = 5, pady = 5)
 
 #실행 프레임
